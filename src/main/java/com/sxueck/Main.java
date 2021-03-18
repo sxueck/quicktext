5445e328bf82f33d6fbbf06f4ea48d835b034263
sxueck <shenyuning@ctirobot.com>
2021-03-18 13:31:08 +0800

    [fix] rebuild

--- src/main/java/com/sxueck/Main.java
+++ src/main/java/com/sxueck/Main.java
@@ -2,10 +2,13 @@ package com.sxueck;
 
 import com.sxueck.netcat.Sockets;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	    Sockets ss = new Sockets();
-	    ss.StartServer();
+	    ss.SetPort(8919);
+	    ss.Start();
     }
 }
--- src/main/java/com/sxueck/netcat/Sockets.java
+++ src/main/java/com/sxueck/netcat/Sockets.java
@@ -1,29 +1,50 @@
 package com.sxueck.netcat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.Socket;
+import java.io.PrintStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Sockets {
-    public void StartServer() {
-        int port = 8919;
-        int len;
-        try {
-            ServerSocket server = new ServerSocket(port);
-            Socket socket = server.accept();
-            Reader reader = new InputStreamReader(socket.getInputStream());
-            char[] chars = new char[1024];
-            StringBuilder builder = new StringBuilder();
-            while ((len = reader.read(chars)) != -1) {
-                builder.append(new String(chars, 0, len));
-            }
-            System.out.println("Receive from client message=: " + builder);
-            reader.close();
-            socket.close();
-            server.close();
-        } catch (Exception ignored) {
 
+    private int PORT;
+
+    public void SetPort(int port) {
+        this.PORT = port;
+    }
+
+    public void Start() throws IOException {
+        System.out.printf("=> listen on 0.0.0.0:%d\n", PORT);
+        ServerSocket server = new ServerSocket(PORT);
+
+        for (; ; ) {
+            try (Socket clientSocket = server.accept()) {
+                handleNewClient(clientSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void handleNewClient(Socket clientSocket) throws IOException {
+        BufferedReader is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintStream os = new PrintStream(clientSocket.getOutputStream());
+        String line;
+        while ((line = is.readLine()) != null) {
+            if (line.equalsIgnoreCase("QUIT")) {
+                break;
+            } else if (line.equalsIgnoreCase("PRINT")) {
+                os.println(line);
+            } else {
+                System.out.println(line);
+                os.println("successfully submitted");
+                os.flush();
+            }
+        }
+        os.flush();
+        is.close();
+        os.close();
     }
 }
